0917672fd6f1c169a1c9cb3f299eedb82b527b21
Jiri Teplicky <jiri.teplicky@unicornuniversity.net>
2022-12-04 21:33:34 +0100

    study programme types

--- uu_subjectman_maing01-server/app/api/validation_types/study-programme-types.js
+++ uu_subjectman_maing01-server/app/api/validation_types/study-programme-types.js
@@ -5,3 +5,23 @@ const studyProgrammeCreateDtoInType = shape({
   description: uu5String(4000),
   degreeOfStudy: uu5String(255).isRequired(),
 });
+
+const studyProgrammeUpdateDToInType = shape({
+  id: id().isRequired(),
+  name: uu5String(255).isRequired(),
+  description: uu5String(4000),
+  degreeOfStudy: oneOf(["ING", "BC", "MGR", "PHDR", "RNDR"]).isRequired(),
+});
+
+const studyProgrammeListDtoInType = shape({
+  sortBy: oneOf(["name", "degreeOfStudy"]),
+  order: oneOf(["asc", "desc"]),
+  pageInfo: shape({
+    pageIndex: integer(),
+    pageSize: integer(),
+  }),
+});
+
+const studyProgrammeGetDToInType = shape({
+  id: id().isRequired(),
+});
